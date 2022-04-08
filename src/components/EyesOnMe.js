c9a94e65cfb1466dde0d04f8f180239d86687c54
vatimetou ebeker <fatimaabcar2@gmail.com>
2022-04-08 17:21:27 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
+import React ,{useState} from 'react';
+
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    const [isFocus,setFocus] =useState(true)
+    console.log({isFocus,setFocus})
+
+    function HandelFocus(){
+        console.log('Good!')
+        
+    
+    }
+    function handelBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+
+    return(
+        <button onFocus={HandelFocus} onBlur={handelBlur} >Eyes on me</button>
+    )
+    }
+    export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+function Keypad(){
+    function handleChange(event) {
+        console.log('Entering password...')
+      }
+return(
+    <input type="password" placeholder=" Password..." onChange={handleChange} />
+)
+}
+export default Keypad
